4b75311ec52f97bc7acc51228bcb3d0f7e4431c6
Matias Fontanini <matias.fontanini@gmail.com>
2018-04-23 19:16:13 -0700

    Print amount of time taken by each test case

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,7 +1,11 @@
+#include <chrono>
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
 using std::string;
+using std::chrono::steady_clock;
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
 
 using Catch::ConsoleReporter;
 using Catch::ReporterConfig;
@@ -15,6 +19,8 @@ namespace cppkafka {
 
 class InstantTestReporter : public ConsoleReporter {
 public:
+    using ClockType = steady_clock;
+
     InstantTestReporter(const ReporterConfig& config)
     : ConsoleReporter(config) {
     }
@@ -32,14 +38,19 @@ public:
     void testCaseStarting(const TestCaseInfo& info) override {
         ConsoleReporter::testCaseStarting(info);
         stream << "Running test \"" << info.name << "\" @ " << info.lineInfo << "\n";
+        test_start_ts_ = ClockType::now();
     }
 
     void testCaseEnded(const TestCaseStats& stats) override {
         const Totals& totals = stats.totals;
         const size_t totalTestCases = totals.assertions.passed + totals.assertions.failed;
+        const auto elapsed = ClockType::now() - test_start_ts_;
         stream << "Done. " << totals.assertions.passed << "/" << totalTestCases
-               << " assertions succeeded\n";
+               << " assertions succeeded in " <<  duration_cast<milliseconds>(elapsed).count()
+               << "ms\n";
     }
+private:
+    ClockType::time_point test_start_ts_;
 };
 
 CATCH_REGISTER_REPORTER("instant", InstantTestReporter)
