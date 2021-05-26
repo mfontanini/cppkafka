36889fcaebf94802a3697ebfc5c1c719f7c96152
illidansr <sergei.ma@outlook.com>
2021-05-26 09:44:04 +0300

    fix : remove bin dir track

--- cmake-build-debug/examples/CMakeFiles/examples.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/examples.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/rdkafka_version_test.cpp
+++
@@ -1,4 +0,0 @@
-#include <librdkafka/rdkafka.h>
- #if RD_KAFKA_VERSION >= 0x00090400
- int main() { }
- #endif
